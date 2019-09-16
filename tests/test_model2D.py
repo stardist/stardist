2e243985f496c7a62dd321cdcce47cb213e8be1e
Uwe Schmidt <uschmidt@mpi-cbg.de>
2019-09-16 17:16:27 +0200

    add (failing) test for multi-channel input

--- tests/test_model2D.py
+++ tests/test_model2D.py
@@ -7,23 +7,30 @@ import tempfile
 
 
 
-@pytest.mark.parametrize('n_rays, grid', [(17,(1,1)),(32,(2,4))])
-def test_model(n_rays, grid):
+@pytest.mark.parametrize('n_rays, grid, n_channel', [(17,(1,1),None), (32,(2,4),1), (4,(8,2),2)])
+def test_model(n_rays, grid, n_channel):
     img = circle_image()
-    imgs = np.repeat(img[np.newaxis],10, axis = 0)
+    imgs = np.repeat(img[np.newaxis], 10, axis=0)
+
+    if n_channel is not None:
+        imgs = np.repeat(imgs[...,np.newaxis], n_channel, axis=-1)
+    else:
+        n_channel = 1
 
     X = imgs+.6*np.random.uniform(0,1,imgs.shape)
-    Y = imgs.astype(int)
+    Y = (imgs if imgs.ndim==3 else imgs[...,0]).astype(int)
 
     conf = Config2D (
-        n_rays       = n_rays,
-        grid         = grid,
-        use_gpu      = False,
-        train_epochs     = 1,
+        n_rays                = n_rays,
+        grid                  = grid,
+        n_channel_in          = n_channel,
+        use_gpu               = False,
+        train_epochs          = 1,
         train_steps_per_epoch = 10,
-        train_loss_weights = (4,1),
-        train_patch_size = (128,128),
-        n_channel_in = 1)
+        train_batch_size      = 2,
+        train_loss_weights    = (4,1),
+        train_patch_size      = (128,128),
+    )
 
     with tempfile.TemporaryDirectory() as tmp:
         model = StarDist2D(conf, name='stardist', basedir=tmp)
