d99fe32a932bc54d0046795d9d7d9586f2a71242
Uwe Schmidt <uschmidt@mpi-cbg.de>
2019-09-17 14:27:48 +0200

    update tests also for 3D

--- tests/test_model2D.py
+++ tests/test_model2D.py
@@ -3,14 +3,12 @@ import numpy as np
 import pytest
 from stardist.models import Config2D, StarDist2D
 from utils import circle_image
-import tempfile
-
 
 
 @pytest.mark.parametrize('n_rays, grid, n_channel', [(17,(1,1),None), (32,(2,4),1), (4,(8,2),2)])
-def test_model(n_rays, grid, n_channel):
-    img = circle_image()
-    imgs = np.repeat(img[np.newaxis], 10, axis=0)
+def test_model(tmpdir, n_rays, grid, n_channel):
+    img = circle_image(shape=(160,160))
+    imgs = np.repeat(img[np.newaxis], 3, axis=0)
 
     if n_channel is not None:
         imgs = np.repeat(imgs[...,np.newaxis], n_channel, axis=-1)
@@ -26,15 +24,14 @@ def test_model(n_rays, grid, n_channel):
         n_channel_in          = n_channel,
         use_gpu               = False,
         train_epochs          = 1,
-        train_steps_per_epoch = 10,
+        train_steps_per_epoch = 2,
         train_batch_size      = 2,
         train_loss_weights    = (4,1),
         train_patch_size      = (128,128),
     )
 
-    with tempfile.TemporaryDirectory() as tmp:
-        model = StarDist2D(conf, name='stardist', basedir=tmp)
-        model.train(X, Y, validation_data=(X[:3],Y[:3]))
+    model = StarDist2D(conf, name='stardist', basedir=str(tmpdir))
+    model.train(X, Y, validation_data=(X[:2],Y[:2]))
 
 
 
--- tests/test_model3D.py
+++ tests/test_model3D.py
@@ -3,30 +3,35 @@ import numpy as np
 import pytest
 from stardist.models import Config3D, StarDist3D
 from utils import circle_image
-import tempfile
 
 
-@pytest.mark.parametrize('n_rays, grid', [(73,(2,2,2)),(33,(1,2,4))])
-def test_model(n_rays, grid):
-    img = circle_image(shape = (64,80,96))
-    imgs = np.repeat(img[np.newaxis],10, axis = 0)
+@pytest.mark.parametrize('n_rays, grid, n_channel', [(73,(2,2,2),None), (33,(1,2,4),1), (7,(2,1,1),2)])
+def test_model(tmpdir, n_rays, grid, n_channel):
+    img = circle_image(shape=(64,80,96))
+    imgs = np.repeat(img[np.newaxis], 3, axis=0)
+
+    if n_channel is not None:
+        imgs = np.repeat(imgs[...,np.newaxis], n_channel, axis=-1)
+    else:
+        n_channel = 1
 
     X = imgs+.6*np.random.uniform(0,1,imgs.shape)
-    Y = imgs.astype(np.uint16)
+    Y = (imgs if imgs.ndim==4 else imgs[...,0]).astype(int)
 
     conf = Config3D (
-        rays       = n_rays,
-        grid       = grid,
-        use_gpu    = False,
-        train_epochs     = 1,
+        rays                  = n_rays,
+        grid                  = grid,
+        n_channel_in          = n_channel,
+        use_gpu               = False,
+        train_epochs          = 1,
         train_steps_per_epoch = 2,
-        train_loss_weights = (4,1),
-        train_patch_size = (48,64,64),
-        n_channel_in = 1)
+        train_batch_size      = 2,
+        train_loss_weights    = (4,1),
+        train_patch_size      = (48,64,64),
+    )
 
-    with tempfile.TemporaryDirectory() as tmp:
-        model = StarDist3D(conf, name='stardist', basedir=tmp)
-        model.train(X, Y, validation_data=(X[:2],Y[:2]))
+    model = StarDist3D(conf, name='stardist', basedir=str(tmpdir))
+    model.train(X, Y, validation_data=(X[:2],Y[:2]))
 
 
 
