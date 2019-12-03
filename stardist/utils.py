0733932a097ec206386ff4bc6621db4623fc9ea1
Uwe Schmidt <uschmidt@mpi-cbg.de>
2019-12-03 21:22:50 +0100

    make edt_prob work for constant label images (with warning)

--- stardist/utils.py
+++ stardist/utils.py
@@ -47,17 +47,29 @@ def _normalize_grid(grid,n):
         raise ValueError("grid must be a list/tuple of length {n} with values that are power of 2".format(n=n))
 
 
-def _edt_prob(lbl_img, anisotropy=None):
+def _edt_dist_func(anisotropy):
     try:
         from edt import edt as edt_func
+        # raise ImportError()
         dist_func = lambda img: edt_func(img>0, anisotropy=anisotropy)
     except ImportError:
         dist_func = lambda img: distance_transform_edt(img, sampling=anisotropy)
+    return dist_func
+
+
+def _edt_prob(lbl_img, anisotropy=None):
+    constant_img = lbl_img.min() == lbl_img.max() and lbl_img.flat[0] > 0
+    if constant_img:
+        lbl_img = np.pad(lbl_img, ((1,)*lbl_img.ndim,)*lbl_img.ndim, mode='constant')
+        warnings.warn("EDT of constant label image is ill-defined. (Assuming background around it.)")
+    dist_func = _edt_dist_func(anisotropy)
     prob = np.zeros(lbl_img.shape,np.float32)
     for l in (set(np.unique(lbl_img)) - set([0])):
         mask = lbl_img==l
         edt = dist_func(mask)[mask]
         prob[mask] = edt/(np.max(edt)+1e-10)
+    if constant_img:
+        prob = prob[(slice(1,-1),)*lbl_img.ndim].copy()
     return prob
 
 
@@ -67,12 +79,11 @@ def edt_prob(lbl_img, anisotropy=None):
         return tuple(slice(s.start-int(w[0]),s.stop+int(w[1])) for s,w in zip(sl,interior))
     def shrink(interior):
         return tuple(slice(int(w[0]),(-1 if w[1] else None)) for w in interior)
-
-    try:
-        from edt import edt as edt_func
-        dist_func = lambda img: edt_func(img>0, anisotropy=anisotropy)
-    except ImportError:
-        dist_func = lambda img: distance_transform_edt(img, sampling=anisotropy)
+    constant_img = lbl_img.min() == lbl_img.max() and lbl_img.flat[0] > 0
+    if constant_img:
+        lbl_img = np.pad(lbl_img, ((1,)*lbl_img.ndim,)*lbl_img.ndim, mode='constant')
+        warnings.warn("EDT of constant label image is ill-defined. (Assuming background around it.)")
+    dist_func = _edt_dist_func(anisotropy)
     objects = find_objects(lbl_img)
     prob = np.zeros(lbl_img.shape,np.float32)
     for i,sl in enumerate(objects,1):
@@ -88,6 +99,8 @@ def edt_prob(lbl_img, anisotropy=None):
         mask = grown_mask[shrink_slice]
         edt = dist_func(grown_mask)[shrink_slice][mask]
         prob[sl][mask] = edt/(np.max(edt)+1e-10)
+    if constant_img:
+        prob = prob[(slice(1,-1),)*lbl_img.ndim].copy()
     return prob
 
 
