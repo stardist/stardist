5fb5f2f3c24aeb237d113d4543cf9792ace89e2c
Martin Weigert <marweigert@gmail.com>
2020-05-11 21:08:18 +0200

    Change output dict for model3d (includes now rays)

--- tests/test_model3D.py
+++ tests/test_model3D.py
@@ -151,10 +151,6 @@ def test_mesh_export():
     labels, polys = model.predict_instances(x, nms_thresh=.5,
                                         overlap_label=-3)
 
-    rays  = rays_from_json(model.config.rays_json)
-    polys["rays_vertices"] = rays.vertices
-    polys["rays_faces"] = rays.faces
-
     s = export_to_obj_file3D(polys,
                              "mesh.obj",scale = (.2,.1,.1))
     return s
