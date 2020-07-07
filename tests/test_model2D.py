9128bc008e9f2496d506e3b860848ca1edbf655c
Uwe Schmidt <uschmidt@mpi-cbg.de>
2020-07-07 15:40:30 +0200

    Remove duplicate test

--- tests/test_model2D.py
+++ tests/test_model2D.py
@@ -159,51 +159,8 @@ def test_pretrained_scales():
     accs = tuple(matching(mask, pred_scale(s)).accuracy for s in scales)
     print("scales   ", np.round(scales,2))
     print("accuracy ", np.round(accs,2))
-    
+
     return accs
-
-
-@pytest.mark.parametrize('n_rays, grid, n_channel', [(17, (1, 1), None), (32, (2, 4), 1), (4, (8, 2), 2)])
-def test_model(tmpdir, n_rays, grid, n_channel):
-    img = circle_image(shape=(160, 160))
-    imgs = np.repeat(img[np.newaxis], 3, axis=0)
-
-    if n_channel is not None:
-        imgs = np.repeat(imgs[..., np.newaxis], n_channel, axis=-1)
-    else:
-        n_channel = 1
-
-    X = imgs+.6*np.random.uniform(0, 1, imgs.shape)
-    Y = (imgs if imgs.ndim == 3 else imgs[..., 0]).astype(int)
-
-    conf = Config2D(
-        n_rays=n_rays,
-        grid=grid,
-        n_channel_in=n_channel,
-        use_gpu=False,
-        train_epochs=1,
-        train_steps_per_epoch=2,
-        train_batch_size=2,
-        train_loss_weights=(4, 1),
-        train_patch_size=(128, 128),
-    )
-
-    model = StarDist2D(conf, name='stardist', basedir=str(tmpdir))
-    model.train(X, Y, validation_data=(X[:2], Y[:2]))
-    ref = model.predict(X[0])
-    res = model.predict(X[0], n_tiles=(
-        (2, 3) if X[0].ndim == 2 else (2, 3, 1)))
-    # assert all(np.allclose(u,v) for u,v in zip(ref,res))
-
-    # ask to train only with foreground patches when there are none
-    # include a constant label image that must trigger a warning
-    conf.train_foreground_only = 1
-    conf.train_steps_per_epoch = 1
-    _X = X[:2]
-    _Y = [np.zeros_like(Y[0]), np.ones_like(Y[1])]
-    with pytest.warns(UserWarning):
-        StarDist2D(conf, name='stardist', basedir=None).train(
-            _X, _Y, validation_data=(X[-1:], Y[-1:]))
 
 
 def test_stardistdata_sequence():
