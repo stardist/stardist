a3efdad0543f60cb67378baa165b1f06aff86b2c
Martin Weigert <marweigert@gmail.com>
2020-07-28 15:18:32 +0200

    Add surface/volume calculations to rays

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,18 +9,24 @@ def pytest_addoption(parser):
 def use_gpu(request):
     return request.config.getoption("--use_gpu")
 
-
-@pytest.fixture(scope='session')
-def model2d():
+def _model2d():
     from utils import path_model2d
     from stardist.models import StarDist2D
     model_path = path_model2d()
     return StarDist2D(None, name=model_path.name, basedir=str(model_path.parent))
 
-
 @pytest.fixture(scope='session')
-def model3d():
+def model2d():
+    return _model2d()
+
+
+def _model3d():
     from utils import path_model3d
     from stardist.models import StarDist3D
     model_path = path_model3d()
     return StarDist3D(None, name=model_path.name, basedir=str(model_path.parent))
+
+
+@pytest.fixture(scope='session')
+def model3d():
+    return _model3d()
