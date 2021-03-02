884fad351a04320a86a188775c9b484923d55281
Martin Weigert <marweigert@gmail.com>
2021-03-02 10:01:48 +0100

    Allow multiple workers in training

--- tests/test_model2D.py
+++ tests/test_model2D.py
@@ -10,8 +10,8 @@ from csbdeep.utils import normalize
 from utils import circle_image, real_image2d, path_model2d, NumpySequence
     
 
-@pytest.mark.parametrize('n_rays, grid, n_channel, use_sequence', [(17, (1, 1), None, False), (32, (2, 4), 1, False), (4, (8, 2), 2, True)])
-def test_model(tmpdir, n_rays, grid, n_channel, use_sequence):
+@pytest.mark.parametrize('n_rays, grid, n_channel, use_sequence, workers', [(17, (1, 1), None, False, 1), (32, (2, 4), 1, False, 4), (4, (8, 2), 2, True, 2)])
+def test_model(tmpdir, n_rays, grid, n_channel, use_sequence, workers):
     img = circle_image(shape=(160, 160))
     imgs = np.repeat(img[np.newaxis], 3, axis=0)
 
@@ -41,7 +41,7 @@ def test_model(tmpdir, n_rays, grid, n_channel, use_sequence):
     )
 
     model = StarDist2D(conf, name='stardist', basedir=str(tmpdir))
-    model.train(X, Y, validation_data=(X[:2], Y[:2]))
+    model.train(X, Y, validation_data=(X[:2], Y[:2]), workers=workers)
     ref = model.predict(X[0])
     res = model.predict(X[0], n_tiles=(
         (2, 3) if X[0].ndim == 2 else (2, 3, 1)))
@@ -179,6 +179,43 @@ def test_stardistdata_sequence():
     return (img,), (prob, dist), s
 
 
+def test_stardistdata_multithreaded(workers=5):
+    np.random.seed(42)
+    from stardist.models import StarDistData2D
+    from scipy.ndimage import rotate
+    from concurrent.futures import ThreadPoolExecutor
+    from time import sleep
+
+    def augmenter(x,y):
+        deg = int(np.sum(y)%117)
+        print(deg)
+        # return x,y
+        x = rotate(x, deg, reshape=False, order=0)
+        y = rotate(y, deg, reshape=False, order=0)
+        # sleep(np.abs(deg)/180)
+        return x,y
+
+    n_samples = 4
+    
+    _ , mask = real_image2d()
+    Y = np.stack([mask+i for i in range(n_samples)])
+    s = StarDistData2D(Y.astype(np.float32), Y,
+                       grid = (1,1),
+                       n_classes = None, augmenter=augmenter,
+                       batch_size=1, patch_size=mask.shape, n_rays=32, length=len(Y))
+
+    a1, b1 = tuple(zip(*tuple(s[i] for i in range(n_samples))))
+
+    with ThreadPoolExecutor(max_workers=n_samples) as e:
+        a2, b2 = tuple(zip(*tuple(e.map(lambda i: s[i], range(n_samples)))))
+
+    assert all([np.allclose(_r1[0], _r2[0]) for _r1,_r2 in zip(a1, a2)])
+    assert all([np.allclose(_r1[0], _r2[0]) for _r1,_r2 in zip(b1, b2)])
+    assert all([np.allclose(_r1[1], _r2[1]) for _r1,_r2 in zip(b1, b2)])    
+    return a2, b2, s
+
+
+
 def test_imagej_rois_export(tmpdir, model2d):
     img = normalize(real_image2d()[0], 1, 99.8)
     labels, polys = model2d.predict_instances(img)
@@ -213,4 +250,6 @@ if __name__ == '__main__':
     # im = render_label_pred_example(model2d())
     # accs = test_pretrained_scales()
 
-    test_foreground_warning()
+    # test_foreground_warning()
+
+    test_model("tmpdir", 32, (1, 1), 1, False,4)
