fc903e964d9f64e3563ba4eecde0c4d80bb44605
Martin Weigert <marweigert@gmail.com>
2021-03-12 20:19:45 +0100

    Small change to Readme; Fix urls; Add doc

--- stardist/data/__init__.py
+++ stardist/data/__init__.py
@@ -1,17 +1,24 @@
-
-
 def abspath(path):
     import os
     base_path = os.path.abspath(os.path.dirname(__file__))
     return os.path.join(base_path, path)
 
-def test_image_nuclei_2d():
+
+def test_image_nuclei_2d(return_mask=False):
     from tifffile import imread
-    img = imread(abspath("images/dsb_test_image.tif"))
-    return img
+    img = imread(abspath("images/img2d.tif"))
+    mask = imread(abspath("images/mask2d.tif"))
+    if return_mask:
+        return img, mask
+    else:
+        return img
 
 
-def test_image_nuclei_3d():
+def test_image_nuclei_3d(return_mask=False):
     from tifffile import imread
-    img = imread(abspath("images/test_stack_3d.tif"))
-    return img
+    img = imread(abspath("images/img3d.tif"))
+    mask = imread(abspath("images/mask3d.tif"))
+    if return_mask:
+        return img, mask
+    else:
+        return img
