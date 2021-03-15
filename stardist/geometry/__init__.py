12e2c63c05a78b646aa1216b19ea80c7052640de
Uwe Schmidt <mail@uweschmidt.org>
2021-03-15 14:46:44 +0100

    Fix 'other2D' notebooks

--- stardist/geometry/__init__.py
+++ stardist/geometry/__init__.py
@@ -5,5 +5,7 @@ from __future__ import absolute_import, print_function
 from .geom2d import star_dist, relabel_image_stardist, ray_angles, dist_to_coord, polygons_to_label, polygons_to_label_coord
 from .geom3d import star_dist3D, polyhedron_to_label, relabel_image_stardist3D, dist_to_coord3D, export_to_obj_file3D
 
+from .geom2d import _dist_to_coord_old, _polygons_to_label_old
+
 
 #, dist_to_volume, dist_to_centroid
