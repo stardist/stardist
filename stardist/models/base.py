0b3e57950322ffda93af5d3bd72b37eda73f7a21
Uwe Schmidt <mail@uweschmidt.org>
2021-05-06 16:44:09 +0200

    Refactor model.predict and model.predict_sparse

--- stardist/models/base.py
+++ stardist/models/base.py
@@ -354,37 +354,8 @@ class StarDistBase(BaseModel):
         self._model_prepared = True
 
 
-    def predict(self, img, axes=None, normalizer=None, n_tiles=None, show_tile_progress=True, **predict_kwargs):
-        """Predict.
-
-        Parameters
-        ----------
-        img : :class:`numpy.ndarray`
-            Input image
-        axes : str or None
-            Axes of the input ``img``.
-            ``None`` denotes that axes of img are the same as denoted in the config.
-        normalizer : :class:`csbdeep.data.Normalizer` or None
-            (Optional) normalization of input image before prediction.
-            Note that the default (``None``) assumes ``img`` to be already normalized.
-        n_tiles : iterable or None
-            Out of memory (OOM) errors can occur if the input image is too large.
-            To avoid this problem, the input image is broken up into (overlapping) tiles
-            that are processed independently and re-assembled.
-            This parameter denotes a tuple of the number of tiles for every image axis (see ``axes``).
-            ``None`` denotes that no tiling should be used.
-        show_tile_progress: bool
-            Whether to show progress during tiled prediction.
-        predict_kwargs: dict
-            Keyword arguments for ``predict`` function of Keras model.
-
-        Returns
-        -------
-        (:class:`numpy.ndarray`, :class:`numpy.ndarray`, [:class:`numpy.ndarray`])
-            Returns the tuple (`prob`, `dist`, [`prob_class`]) of per-pixel object probabilities and star-convex polygon/polyhedra distances.
-            In multiclass prediction mode, `prob_class` is the probability map for each of the 1+'n_classes' classes (first class is background)
-
-        """
+    def _predict_setup(self, img, axes, normalizer, n_tiles, show_tile_progress, predict_kwargs):
+        """ Shared setup code between `predict` and `predict_sparse` """
         if n_tiles is None:
             n_tiles = [1]*img.ndim
         try:
@@ -420,42 +391,86 @@ class StarDistBase(BaseModel):
         if not _is_floatarray(x):
             warnings.warn("Predicting on non-float input... ( forgot to normalize? )")
 
-        def predict_direct(tile):
-            xs = self.keras_model.predict(tile[np.newaxis], **predict_kwargs)
-            return tuple(x[0] for x in xs)
+        def predict_direct(x):
+            ys = self.keras_model.predict(x[np.newaxis], **predict_kwargs)
+            return tuple(y[0] for y in ys)
 
-        if np.prod(n_tiles) > 1:
+        def tiling_setup():
+            assert np.prod(n_tiles) > 1
             tiling_axes   = axes_net.replace('C','') # axes eligible for tiling
             x_tiling_axis = tuple(axes_dict(axes_net)[a] for a in tiling_axes) # numerical axis ids for x
             axes_net_tile_overlaps = self._axes_tile_overlap(axes_net)
             # hack: permute tiling axis in the same way as img -> x was permuted
-            n_tiles = _permute_axes(np.empty(n_tiles,np.bool)).shape
-            (all(n_tiles[i] == 1 for i in range(x.ndim) if i not in x_tiling_axis) or
+            _n_tiles = _permute_axes(np.empty(n_tiles,np.bool)).shape
+            (all(_n_tiles[i] == 1 for i in range(x.ndim) if i not in x_tiling_axis) or
                 _raise(ValueError("entry of n_tiles > 1 only allowed for axes '%s'" % tiling_axes)))
 
             sh = [s//grid_dict.get(a,1) for a,s in zip(axes_net,x.shape)]
-
-            sh[channel] = 1;
-            prob = np.empty(sh,np.float32)
-
-            sh[channel] = self.config.n_rays;
-            dist = np.empty(sh,np.float32)
-
-            if self._is_multiclass():
-                sh[channel] = self.config.n_classes+1;
-                prob_class = np.empty(sh,np.float32)
-                result = (prob, dist, prob_class)
-            else:
-                result = (prob, dist)
+            sh[channel] = None
+            def create_empty_output(n_channel, dtype=np.float32):
+                sh[channel] = n_channel
+                return np.empty(sh,dtype)
 
             n_block_overlaps = [int(np.ceil(overlap/blocksize)) for overlap, blocksize
                                 in zip(axes_net_tile_overlaps, axes_net_div_by)]
 
-            num_tiles_used = total_n_tiles(x, n_tiles, block_sizes=axes_net_div_by, n_block_overlaps=n_block_overlaps)
+            num_tiles_used = total_n_tiles(x, _n_tiles, block_sizes=axes_net_div_by, n_block_overlaps=n_block_overlaps)
 
-            for tile, s_src, s_dst in tqdm(tile_iterator(x, n_tiles, block_sizes=axes_net_div_by, n_block_overlaps=n_block_overlaps),
-                                           disable=(not show_tile_progress), total=num_tiles_used):
+            tile_generator = tqdm(tile_iterator(x, _n_tiles, block_sizes=axes_net_div_by, n_block_overlaps=n_block_overlaps),
+                                                disable=(not show_tile_progress), total=num_tiles_used)
 
+            return tile_generator, tuple(sh), create_empty_output
+
+        return x, axes, axes_net, axes_net_div_by, _permute_axes, resizer, n_tiles, grid, grid_dict, channel, predict_direct, tiling_setup
+
+
+    def predict(self, img, axes=None, normalizer=None, n_tiles=None, show_tile_progress=True, **predict_kwargs):
+        """Predict.
+
+        Parameters
+        ----------
+        img : :class:`numpy.ndarray`
+            Input image
+        axes : str or None
+            Axes of the input ``img``.
+            ``None`` denotes that axes of img are the same as denoted in the config.
+        normalizer : :class:`csbdeep.data.Normalizer` or None
+            (Optional) normalization of input image before prediction.
+            Note that the default (``None``) assumes ``img`` to be already normalized.
+        n_tiles : iterable or None
+            Out of memory (OOM) errors can occur if the input image is too large.
+            To avoid this problem, the input image is broken up into (overlapping) tiles
+            that are processed independently and re-assembled.
+            This parameter denotes a tuple of the number of tiles for every image axis (see ``axes``).
+            ``None`` denotes that no tiling should be used.
+        show_tile_progress: bool
+            Whether to show progress during tiled prediction.
+        predict_kwargs: dict
+            Keyword arguments for ``predict`` function of Keras model.
+
+        Returns
+        -------
+        (:class:`numpy.ndarray`, :class:`numpy.ndarray`, [:class:`numpy.ndarray`])
+            Returns the tuple (`prob`, `dist`, [`prob_class`]) of per-pixel object probabilities and star-convex polygon/polyhedra distances.
+            In multiclass prediction mode, `prob_class` is the probability map for each of the 1+'n_classes' classes (first class is background)
+
+        """
+
+        x, axes, axes_net, axes_net_div_by, _permute_axes, resizer, n_tiles, grid, grid_dict, channel, predict_direct, tiling_setup = \
+            self._predict_setup(img, axes, normalizer, n_tiles, show_tile_progress, predict_kwargs)
+
+        if np.prod(n_tiles) > 1:
+            tile_generator, output_shape, create_empty_output = tiling_setup()
+
+            prob = create_empty_output(1)
+            dist = create_empty_output(self.config.n_rays)
+            if self._is_multiclass():
+                prob_class = create_empty_output(self.config.n_classes+1)
+                result = (prob, dist, prob_class)
+            else:
+                result = (prob, dist)
+
+            for tile, s_src, s_dst in tile_generator:
                 # predict_direct -> prob, dist, [prob_class if multi_class]
                 result_tile = predict_direct(tile)
                 # account for grid
@@ -468,7 +483,6 @@ class StarDistBase(BaseModel):
                 # print(s_src,s_dst)
                 for part, part_tile in zip(result, result_tile):
                     part[s_dst] = part_tile[s_src]
-
         else:
             # predict_direct -> prob, dist, [prob_class if multi_class]
             result = predict_direct(x)
@@ -491,48 +505,15 @@ class StarDistBase(BaseModel):
 
 
     def predict_sparse(self, img, prob_thresh=None, axes=None, normalizer=None, n_tiles=None, show_tile_progress=True, b=2, **predict_kwargs):
-        """sparse version of model.predict() 
-        TODO: refactor common funcs with model.predict()
+        """ Sparse version of model.predict()
         Returns
         -------
         (prob, dist, [prob_class], points)   flat list of probs, dists, (optional prob_class) and points
         """
         if prob_thresh is None: prob_thresh = self.thresholds.prob
-               
-        if n_tiles is None:
-            n_tiles = [1]*img.ndim
-        try:
-            n_tiles = tuple(n_tiles)
-            img.ndim == len(n_tiles) or _raise(TypeError())
-        except TypeError:
-            raise ValueError("n_tiles must be an iterable of length %d" % img.ndim)
-        all(np.isscalar(t) and 1<=t and int(t)==t for t in n_tiles) or _raise(
-            ValueError("all values of n_tiles must be integer values >= 1"))
-        n_tiles = tuple(map(int,n_tiles))
 
-        axes     = self._normalize_axes(img, axes)
-        axes_net = self.config.axes
-
-        _permute_axes = self._make_permute_axes(axes, axes_net)
-        x = _permute_axes(img) # x has axes_net semantics
-
-        channel = axes_dict(axes_net)['C']
-        self.config.n_channel_in == x.shape[channel] or _raise(ValueError())
-        axes_net_div_by = self._axes_div_by(axes_net)
-
-        grid = tuple(self.config.grid)
-        len(grid) == len(axes_net)-1 or _raise(ValueError())
-        grid_dict = dict(zip(axes_net.replace('C',''),grid))
-
-        normalizer = self._check_normalizer_resizer(normalizer, None)[0]
-        resizer = StarDistPadAndCropResizer(grid=grid_dict)
-
-        x = normalizer.before(x, axes_net)
-        x = resizer.before(x, axes_net, axes_net_div_by)
-
-        def predict_direct(tile):
-            xs = self.keras_model.predict(tile[np.newaxis],  **predict_kwargs)
-            return tuple(x[0] for x in xs)
+        x, axes, axes_net, axes_net_div_by, _permute_axes, resizer, n_tiles, grid, grid_dict, channel, predict_direct, tiling_setup = \
+            self._predict_setup(img, axes, normalizer, n_tiles, show_tile_progress, predict_kwargs)
 
         def _prep(prob, dist):
             prob = np.take(prob,0,axis=channel)
@@ -540,31 +521,20 @@ class StarDistBase(BaseModel):
             dist = np.maximum(1e-3, dist)
             return prob, dist
 
-
         proba, dista, pointsa, prob_class = [],[],[], []
-        
-        if np.prod(n_tiles) > 1:
-            tiling_axes   = axes_net.replace('C','') # axes eligible for tiling
-            x_tiling_axis = tuple(axes_dict(axes_net)[a] for a in tiling_axes) # numerical axis ids for x
-            axes_net_tile_overlaps = self._axes_tile_overlap(axes_net)
-            # hack: permute tiling axis in the same way as img -> x was permuted
-            n_tiles = _permute_axes(np.empty(n_tiles,np.bool)).shape
-            (all(n_tiles[i] == 1 for i in range(x.ndim) if i not in x_tiling_axis) or
-                _raise(ValueError("entry of n_tiles > 1 only allowed for axes '%s'" % tiling_axes)))
 
-            sh = [s//grid_dict.get(a,1) for a,s in zip(axes_net,x.shape)]
+        if np.prod(n_tiles) > 1:
+            tile_generator, output_shape, create_empty_output = tiling_setup()
+
+            sh = list(output_shape)
             sh[channel] = 1;
 
             proba, dista, pointsa, prob_classa = [], [], [], []
-            
-            n_block_overlaps = [int(np.ceil(overlap/blocksize)) for overlap, blocksize
-                                in zip(axes_net_tile_overlaps, axes_net_div_by)]
 
-            for tile, s_src, s_dst in tqdm(tile_iterator(x, n_tiles, block_sizes=axes_net_div_by, n_block_overlaps=n_block_overlaps),
-                                           disable=(not show_tile_progress), total=np.prod(n_tiles)):
+            for tile, s_src, s_dst in tile_generator:
 
                 results_tile = predict_direct(tile)
-                
+
                 # account for grid
                 s_src = [slice(s.start//grid_dict.get(a,1),s.stop//grid_dict.get(a,1)) for s,a in zip(s_src,axes_net)]
                 s_dst = [slice(s.start//grid_dict.get(a,1),s.stop//grid_dict.get(a,1)) for s,a in zip(s_dst,axes_net)]
@@ -586,15 +556,15 @@ class StarDistBase(BaseModel):
                 _points = _points + np.array(offset).reshape((1,len(offset)))
                 _points = _points * np.array(self.config.grid).reshape((1,len(self.config.grid)))
                 pointsa.extend(_points)
-                
+
                 if self._is_multiclass():
                     p = results_tile[2][s_src].copy()
                     p = np.moveaxis(p,channel,-1)
                     prob_classa.extend(p[inds])
-                                      
+
         else:
             # predict_direct -> prob, dist, [prob_class if multi_class]
-            results = predict_direct(x)            
+            results = predict_direct(x)
             prob, dist = results[:2]
             prob, dist = _prep(prob, dist)
             inds   = _ind_prob_thresh(prob, prob_thresh, b=b)
@@ -607,19 +577,19 @@ class StarDistBase(BaseModel):
                 p = np.moveaxis(results[2],channel,-1)
                 prob_classa = p[inds].copy()
 
-                
+
         proba = np.asarray(proba)
         dista = np.asarray(dista).reshape((-1,self.config.n_rays))
         pointsa = np.asarray(pointsa).reshape((-1,self.config.n_dim))
-        
+
         if self._is_multiclass():
-            prob_classa = np.asarray(prob_classa).reshape((-1,self.config.n_classes+1))            
+            prob_classa = np.asarray(prob_classa).reshape((-1,self.config.n_classes+1))
             return proba, dista, prob_classa, pointsa
         else:
             prob_classa = None
             return proba, dista, pointsa
 
-    
+
     def predict_instances(self, img, axes=None, normalizer=None,
                           sparse = False,  
                           prob_thresh=None, nms_thresh=None,
