733084c8d210cb77485d193371a4b5da067e423f
Uwe Schmidt <mail@uweschmidt.org>
2021-05-08 00:27:49 +0200

    Make sparse=True the default in predict_instances

--- stardist/models/base.py
+++ stardist/models/base.py
@@ -591,7 +591,7 @@ class StarDistBase(BaseModel):
 
 
     def predict_instances(self, img, axes=None, normalizer=None,
-                          sparse=False,
+                          sparse=True,
                           prob_thresh=None, nms_thresh=None,
                           n_tiles=None, show_tile_progress=True,
                           verbose=False,
