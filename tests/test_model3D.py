5c40bdf53793d50c413d39d2bc6963c2eb86fa76
Martin Weigert <martin.weigert@epfl.ch>
2022-02-02 22:16:30 +0100

    Fix sparse prediction bug for multi_class

--- tests/test_model3D.py
+++ tests/test_model3D.py
@@ -362,5 +362,5 @@ def test_load_and_export_TF(model3d):
 if __name__ == '__main__':
     # from conftest import _model3d
     # model, lbl = test_load_and_predict_with_overlap(_model3d())
-    # res = _test_model_multiclass(n_classes = 2, classes="area", n_channel=1, epochs=2)
-    test_stardistdata((2,2,2), True)
+    res = _test_model_multiclass(n_classes = 2, classes="auto", n_channel=1, epochs=20)
+    # test_stardistdata((2,2,2), True)
