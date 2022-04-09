7e7aaac69265226f9d70b714e8397fab0fe0f78b
Uwe Schmidt <mail@uweschmidt.org>
2022-04-10 00:44:16 +0200

    Deprecate Python 3.6

--- stardist/__init__.py
+++ stardist/__init__.py
@@ -19,3 +19,12 @@ from .plot.render import render_label, render_label_pred
 from .rays3d import rays_from_json, Rays_Cartesian, Rays_SubDivide, Rays_Tetra, Rays_Octo, Rays_GoldenSpiral, Rays_Explicit
 from .sample_patches import sample_patches
 from .bioimageio_utils import export_bioimageio, import_bioimageio
+
+def _py_deprecation(ver_python=(3,6), ver_stardist='0.9.0'):
+     import sys
+     from distutils.version import LooseVersion
+     if sys.version_info[:2] == ver_python and LooseVersion(__version__) < LooseVersion(ver_stardist):
+         print(f"You are using Python {'.'.join(map(str,ver_python))}, which will no longer be supported in StarDist {ver_stardist}.\n"
+               f"→ Please upgrade to Python {'.'.join((str(ver_python[0]),str(ver_python[1]+1)))} or later.", file=sys.stderr, flush=True)
+_py_deprecation()
+del _py_deprecation
