b24edb7aa2e4d9d18e0a08cfd9b4162adad81510
Uwe Schmidt <mail@uweschmidt.org>
2022-04-10 01:38:37 +0200

    Add license files for clipper and nanoflann

--- stardist/__init__.py
+++ stardist/__init__.py
@@ -24,7 +24,7 @@ def _py_deprecation(ver_python=(3,6), ver_stardist='0.9.0'):
      import sys
      from distutils.version import LooseVersion
      if sys.version_info[:2] == ver_python and LooseVersion(__version__) < LooseVersion(ver_stardist):
-         print(f"You are using Python {'.'.join(map(str,ver_python))}, which will no longer be supported in StarDist {ver_stardist}.\n"
-               f"→ Please upgrade to Python {'.'.join((str(ver_python[0]),str(ver_python[1]+1)))} or later.", file=sys.stderr, flush=True)
+         print(f"You are using Python {ver_python[0]}.{ver_python[1]}, which will no longer be supported in StarDist {ver_stardist}.\n"
+               f"→ Please upgrade to Python {ver_python[0]}.{ver_python[1]+1} or later.", file=sys.stderr, flush=True)
 _py_deprecation()
 del _py_deprecation
