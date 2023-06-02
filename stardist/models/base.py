aac68890130a95755146a4457339886472557f2d
Utkarsh Kanwat <utkarshkanwat@gmail.com>
2023-06-02 08:26:33 +0530

    Update base.py

--- stardist/models/base.py
+++ stardist/models/base.py
@@ -252,10 +252,10 @@ def compound_tversky_cce(weights, ndim, alpha=0.7, gamma=0):
     def dice_cce(y_true, y_pred):
 
 ## CONIC ----------------------------
-        rep_list = [2,4,8,10,11,12]
+#         rep_list = [2,4,8,10,11,12]
  
-        add_list = [9,10,11,12]
-        y_pred = add_pred_vals(y_pred, add_list, rep_list)
+#         add_list = [9,10,11,12]
+#         y_pred = add_pred_vals(y_pred, add_list, rep_list)
 ## ----------------------------------
         
 ## CoNSeP -------------------------
@@ -269,9 +269,9 @@ def compound_tversky_cce(weights, ndim, alpha=0.7, gamma=0):
 
 
 ## PanNuke -----------------------------
-#         rep_list = [4,5,6,7,8,12]
-#         add_list = [3,4,5,6,7]
-#         y_pred = add_pred_vals(y_pred, add_list, rep_list)
+        rep_list = [4,5,6,7,8,12]
+        add_list = [3,4,5,6,7]
+        y_pred = add_pred_vals(y_pred, add_list, rep_list)
 ## ---------------------------------------
 
 
@@ -689,9 +689,9 @@ class StarDistBase(BaseModel):
 
         
 ## CONIC ----------------------------
-        rep_list = [2,4,8,10,11,12]
-        add_list = [9,10,11,12]
-        result[2] = out_add_pred_vals(result[2], add_list, rep_list)
+#         rep_list = [2,4,8,10,11,12]
+#         add_list = [9,10,11,12]
+#         result[2] = out_add_pred_vals(result[2], add_list, rep_list)
 ## ----------------------------------
         
 ## CoNSeP -------------------------
@@ -705,9 +705,9 @@ class StarDistBase(BaseModel):
 
 
 ## PanNuke -----------------------------
-#         rep_list = [4,5,6,7,8,12]
-#         add_list = [3,4,5,6,7]
-#         result[2] = out_add_pred_vals(result[2], add_list, rep_list)
+        rep_list = [4,5,6,7,8,12]
+        add_list = [3,4,5,6,7]
+        result[2] = out_add_pred_vals(result[2], add_list, rep_list)
 ## ---------------------------------------      
 
 
