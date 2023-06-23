d99ebd201f63876354a2939b597f09f972c18540
ukanwat <utkarshkanwat@gmail.com>
2023-06-23 06:57:22 +0530

    added dep

--- stardist/models/base.py
+++ stardist/models/base.py
@@ -287,32 +287,32 @@ def compound_tversky_cce(weights, ndim, alpha=0.7, gamma=0):
 
     def dice_cce(y_true, y_pred):
         # CONIC ----------------------------
-        rep_list = [2, 4, 8, 10, 11, 12]
+        # rep_list = [2, 4, 8, 10, 11, 12]
 
-        add_list = [9, 10, 11, 12]
-        y_pred = add_pred_vals(y_pred, add_list, rep_list)
+        # add_list = [9, 10, 11, 12]
+        # y_pred = add_pred_vals(y_pred, add_list, rep_list)
         # ----------------------------------
 
         ## CoNSeP -------------------------
-        #         rep_list = [10,11,12]
-        #         add_list = [9,10,11,12]
-        #         y_pred = add_pred_vals(y_pred, add_list, rep_list)
-        #         rep_list = [4,5,6,7]
-        #         add_list = [3,4,5,6,7]
-        #         y_pred = add_pred_vals(y_pred, add_list, rep_list)
+        rep_list = [10,11,12]
+        add_list = [9,10,11,12]
+        y_pred = add_pred_vals(y_pred, add_list, rep_list)
+        rep_list = [4,5,6,7]
+        add_list = [3,4,5,6,7]
+        y_pred = add_pred_vals(y_pred, add_list, rep_list)
         ## --------------------------------
 
-        ## PanNuke -----------------------------
-        #         rep_list = [4,5,6,7,8,12]
-        #         add_list = [3,4,5,6,7]
-        #         y_pred = add_pred_vals(y_pred, add_list, rep_list)
-        ## ---------------------------------------
+## PanNuke -----------------------------
+#         rep_list = [4,5,6,7,8,12]
+#         add_list = [3,4,5,6,7]
+#         y_pred = add_pred_vals(y_pred, add_list, rep_list)
+## ---------------------------------------
 
-        ## CoNuSAC -----------------------------
-        #         rep_list = [2,6,7,8,9,10,11,12]
-        #         add_list = [1,2]
-        #         y_pred = add_pred_vals(y_pred, add_list, rep_list)
-        ## ---------------------------------------
+## CoNuSAC -----------------------------
+#         rep_list = [2,6,7,8,9,10,11,12]
+#         add_list = [1,2]
+#         y_pred = add_pred_vals(y_pred, add_list, rep_list)
+## ---------------------------------------
 
         return K.mean(_cce(y_true, y_pred)) + K.mean(_tversky(y_true, y_pred))
 
@@ -610,7 +610,7 @@ class StarDistBase(BaseModel):
             loss = [prob_loss, dist_loss, prob_class_loss]
         else:
             loss = [prob_loss, dist_loss]
-
+        
         self.keras_model.compile(
             optimizer,
             loss=loss,
@@ -879,19 +879,19 @@ class StarDistBase(BaseModel):
             result[2] = np.moveaxis(result[2], channel, -1)
 
         # CONIC ----------------------------
-        rep_list = [2, 4, 8, 10, 11, 12]
-        add_list = [9, 10, 11, 12]
-        result[2] = out_add_pred_vals(result[2], add_list, rep_list)
+        # rep_list = [2, 4, 8, 10, 11, 12]
+        # add_list = [9, 10, 11, 12]
+        # result[2] = out_add_pred_vals(result[2], add_list, rep_list)
         # ----------------------------------
 
-        ## CoNSeP -------------------------
-        #         rep_list = [10,11,12]
-        #         add_list = [9,10,11,12]
-        #         result[2] = out_add_pred_vals(result[2], add_list, rep_list)
-        #         rep_list = [4,5,6,7]
-        #         add_list = [3,4,5,6,7]
-        #         result[2] = out_add_pred_vals(result[2], add_list, rep_list)
-        ## --------------------------------
+## CoNSeP -------------------------
+        rep_list = [10,11,12]
+        add_list = [9,10,11,12]
+        result[2] = out_add_pred_vals(result[2], add_list, rep_list)
+        rep_list = [4,5,6,7]
+        add_list = [3,4,5,6,7]
+        result[2] = out_add_pred_vals(result[2], add_list, rep_list)
+## --------------------------------
 
         ## PanNuke -----------------------------
         #         rep_list = [4,5,6,7,8,12]
