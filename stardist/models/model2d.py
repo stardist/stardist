97f67ea05a6bd0bcaab2a0e371fabc229ad03eae
Ajinkya Kulkarni <kulkajinkya@gmail.com>
2023-07-01 10:20:16 +0200

    Replaced `from tqdm import tqdm` with `from tqdm.auto import tqdm` for consistent UI across platforms

--- stardist/models/model2d.py
+++ stardist/models/model2d.py
@@ -3,7 +3,7 @@ from __future__ import print_function, unicode_literals, absolute_import, divisi
 import numpy as np
 import warnings
 import math
-from tqdm import tqdm
+from tqdm.auto import tqdm
 
 from csbdeep.models import BaseConfig
 from csbdeep.internals.blocks import unet_block
