05a66b77e2e161420816c6bf8c39561156e31ef4
Uwe Schmidt <mail@uweschmidt.org>
2024-04-20 23:06:59 +0200

    Remove import from numpy.distutils in setup.py
    numpy.distutils has been removed in Python 3.12
    https://numpy.org/doc/stable/reference/distutils_status_migration.html#distutils-status-migration

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, print_function
 from setuptools import setup, find_packages, Extension
 from setuptools.command.build_ext import build_ext
-from numpy.distutils.misc_util import get_numpy_include_dirs
+from numpy import get_include as get_numpy_include_dirs
 from os import path
 from glob import glob
 
