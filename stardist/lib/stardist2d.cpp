678cfeaf21bd0ed92ec8d73512d9d112d886d9a1
Martin Weigert <marweigert@gmail.com>
2025-12-12 17:54:57 +0100

    Fix NULL pointer bug in c_non_max_suppression_inds
    Remove dead code that dereferenced uninitialized mapping variable.
    This was causing segfaults when compiled with clang+libomp.

--- stardist/lib/stardist2d.cpp
+++ stardist/lib/stardist2d.cpp
@@ -389,7 +389,7 @@ static PyObject* c_non_max_suppression_inds_old(PyObject *self, PyObject *args) 
   
 static PyObject* c_non_max_suppression_inds(PyObject *self, PyObject *args) {
 
-  PyArrayObject *dist=NULL,*points_arr=NULL, *mapping=NULL, *result=NULL;
+  PyArrayObject *dist=NULL, *points_arr=NULL, *result=NULL;
   float threshold;
   int verbose, use_kdtree, use_bbox;
 
@@ -398,9 +398,6 @@ static PyObject* c_non_max_suppression_inds(PyObject *self, PyObject *args) {
     return NULL;
 
   const float * const points = (float*) PyArray_DATA(points_arr);
-  
-  npy_intp *img_dims = PyArray_DIMS(mapping);
-  const int height = img_dims[0], width = img_dims[1];
 
   npy_intp *dims = PyArray_DIMS(dist);
   const int n_polys = dims[0];
